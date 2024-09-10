ef48efe6caaabbdc8aafbc756635ce12d0964e6a
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2024-09-10 11:53:47 -0500

    Updated to use pointers for extra credit

--- Week 4/lab3.cpp
+++ Week 4/lab3.cpp
@@ -23,63 +23,74 @@ Use pointers.
 #include <string>
 using namespace std;
 
+string *generatePatternArray();
 void printHeader();
-void printPattern();
+void printPattern(string*);
 void horizontalLine();
-void patternA(int);
-void patternB(int);
+void patternA(int, string*);
+void patternB(int, string*);
+
+const int SIZE = 10;
+const int WIDTH = 50;
 
 int main()
 {
 
+    string* patternArray = generatePatternArray();
+
     printHeader();
 
-    printPattern();
+    printPattern(patternArray);
 
     return 0;
+}
+
+string* generatePatternArray()
+{
+    string* patternArray = new string[SIZE];
+    for (int line = 0; line < SIZE; line++)
+    {
+        string pattern = "";
+        for (int character = 0; character <= line; character++)
+        {
+            pattern += "+";
+        }
+        patternArray[line] = pattern;
+    }
+    return patternArray;
 }
 
 void printHeader()
 {
     horizontalLine();
-    cout << setw(25) << left << "Pattern A" << setw(25) << left << "Pattern B" << endl;
+    cout << setw(WIDTH/2) << left << "Pattern A" << setw(WIDTH/2) << left << "Pattern B" << endl;
     horizontalLine();
 }
 
-void printPattern()
+void printPattern(string *patternArray)
 {
-    for (int line = 1; line <= 10; line++)
+    for (int line = 1; line <= SIZE; line++)
     {
-        patternA(line);
-        patternB(line);
+        patternA(line, patternArray);
+        patternB(line, patternArray);
     }
 }
 
 void horizontalLine()
 {
-    cout << setw(50) << "―――――――――――――――――――――――――――――――――――――――――――――――――――――――" << endl;
+    cout << setw(WIDTH) << "―――――――――――――――――――――――――――――――――――――――――――――――――――――――" << endl;
 }
 
-void patternA(int line)
+void patternA(int line, string *patternArray)
 {
-    string pattern = "";
-    for (int character = 1; character <= line; character++)
-    {
-        pattern += "+";
-    }
-    cout << setw(25) << left << pattern;
+    cout << setw(WIDTH/2) << left << patternArray[line - 1];
 }
 
-void patternB(int line)
+void patternB(int line, string *patternArray)
 {
-    string pattern = "";
-    for (int character = 10; character >= line; character--)
-    {
-        pattern += "+";
-    }
-
-    cout << setw(25) << left << pattern << endl;
-    if(line == 10)
+    cout << setw(WIDTH/2) << left << patternArray[SIZE - line];
+    cout << endl;
+    if(line == SIZE)
     {
         horizontalLine();
     }
